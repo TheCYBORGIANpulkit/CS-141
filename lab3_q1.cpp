06ab0f430211f43c73ecc863f1e7381fc671cf40
PULKIT OJHA <42231120+TheCYBORGIANpulkit@users.noreply.github.com>
2018-08-19 01:04:46 -0400

    Add files via upload

--- lab3_q1.cpp
+++ lab3_q1.cpp
@@ -1,19 +1,28 @@
-# include<iostream>
+# include<iostream>			// to write the main liobrary code
 using namespace std;
-int main () 
-{
-  int a=2;
+
+int main () {				// to declare the primary the function
+  
+int a=2;				// to declare the types of variables
 float P=3.14;
 double G=3.145962;
 char q='b';
-bool statement= (2==3);
-cout << "the value of integer data type is " << a <<endl;
-cout << "the value of float data type is " << P <<endl;
-cout << "the value of double data type is " << G <<endl;
-cout << "the value of char data type is " << q <<endl;
-cout << "the size of integer is " << sizeof(a)<< "bits " << "the value of integer is "<< a <<endl;
-cout << "the size of float is " << sizeof(P)<< "bits " << "the value of float is "<< P  <<endl;
-cout << "the size of double is " << sizeof(G)<< "bits " << "the value of double is "<<G <<endl; 
-cout << "the size of char is " << sizeof(q)<< "bits " << "the value of char is "<< q <<endl;
-cout << "the value of my statement in bool is " << statement << endl; 
+bool statement= (2==3);			
+					// to print the line with the value of the declared 						variable
+
+	cout << "the value of integer data type is " << a <<endl;
+	cout << "the value of float data type is " << P <<endl;		
+	cout << "the value of double data type is " << G <<endl;
+	cout << "the value of char data type is " << q <<endl;
+	cout << endl;
+					// to print the line with the size and the value of 						the declared variable
+
+cout << "the size of integer is " << sizeof(a)<< "bits " << "the value of the integer is "<< a <<endl;
+cout << "the size of float is " << sizeof(P)<< "bits " << "the value of the float is"<< P  <<endl;
+cout << "the size of double is " << sizeof(G)<< "bits " << "the value of the double is "<<G <<endl; 
+cout << "the size of char is " << sizeof(q)<< "bits " << "the value of the char is "<<q <<endl;
+					// to print the line containing the bool variable
+
+	cout << "the value of my statement in bool is " << statement << endl; 
 }
+
