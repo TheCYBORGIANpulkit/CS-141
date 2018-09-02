e79fa4d30a8268dd556e45cd324d361778e01aa7
PULKIT OJHA <42231120+TheCYBORGIANpulkit@users.noreply.github.com>
2018-09-02 12:34:25 +0000

    Update lab5_q24.cpp

--- lab5_q24.cpp
+++ lab5_q24.cpp
@@ -3,10 +3,11 @@ using namespace std;
 
 int main()
 {		//to declare the main variable
-
+		
+		// to print the begining statement
 	cout<< "hello, we are here to print all the alphabets from a to z." <<endl;
-	char i='a';
+	char i='a'; // to declare the variable
 	while(i <= char('z')){
-		cout<< i<<endl;
+		cout<< i<<endl; // to print the alphabets
 		i++;}
 }
