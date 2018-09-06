3ea87acbb9376bcc3291cd08a160a68cb977ae67
PULKIT OJHA <42231120+TheCYBORGIANpulkit@users.noreply.github.com>
2018-09-06 18:46:47 +0530

    Add files via upload

---
+++ lab5.5q1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{	int i=0;
+	for( i=0;i<5;i++)
+	{	cout<<"*";
+		for(int j=0;j<4;j++)
+		{	cout<<"*";
+		}
+			cout<<endl;
+		
+	}
+	return 0;
+}
