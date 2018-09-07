aa79f47f801a000c3af3476c8b0d29258983d106
PULKIT OJHA <42231120+TheCYBORGIANpulkit@users.noreply.github.com>
2018-09-07 04:13:10 +0000

    Update lab5.5q1.cpp

--- lab5.5q1.cpp
+++ lab5.5q1.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 int main()
-{
+{	// to get the length and breadth of the rectangle as input
 	int a,b;
 		cout<<"Please input the rows and columns of the square to be printed"<<endl;
 		cin>> a>> b;	
-	int i=0;
+	int i=0;	//to print the stars in a column
 	for( i=0;i<b;i++)
-	{	cout<<"*";
+	{		//to print the stars in a row for every column 
 			for(int j=0;j<a;j++)
 			{	cout<<"*";
 			}
