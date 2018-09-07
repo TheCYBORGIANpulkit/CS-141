69f0e738abe7fddaca8b19bb2845c92050af1466
PULKIT OJHA <42231120+TheCYBORGIANpulkit@users.noreply.github.com>
2018-09-07 04:53:34 +0000

    Update lab5.5q2.cpp

--- lab5.5q2.cpp
+++ lab5.5q2.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main()
 {
-int n;
+int n;	//to get the height of triangle to be printed as input
 	cout<<"Please input the no. of height of the triangle to be printed"<<endl;
 	cin>> n;
-	int i=0;
+	int i=0; //for n columns do the following
 		for( i=0;i<=n;i++)
-		{	
+		{	//print i-1 stars
 			for(int j=0;j<i;j++)
 			{	cout<<"*";
-			
+			//then end the line
 			}	cout<<endl;	
 		}		
 	return 0;
