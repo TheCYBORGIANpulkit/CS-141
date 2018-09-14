32b3bfae8e2376f8eb0787e46ce8334a236fccbb
PULKIT OJHA <42231120+TheCYBORGIANpulkit@users.noreply.github.com>
2018-09-14 07:09:31 +0000

    Update lab6q2b.cpp

--- lab6q2b.cpp
+++ lab6q2b.cpp
@@ -8,15 +8,16 @@ void funcR(int a, int b, int &t){
 
 //defining the main function
 int main(){
+	//declaring the variables
 	int a,b, sum;
+	//ask the user for input and gather it;
 	cout<< "please input two numbers. We'll sum them."<<endl;
 		cin>>a>>b;
-	//declaring the variable
+	
 	
 	//call the function
 	funcR(a,b,sum);
-		//ask the user for input and gather it;
-		
+		// printing the result statement
 		cout<<"The sum of your numbers is "<< sum<<endl;
 		return 0;
 }
